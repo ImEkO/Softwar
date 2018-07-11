5b1f5a52c2a93637128473512518927246bebb91
Unknown <olivie_a@etna-alternance.net>
2018-07-11 16:42:05 +0200

    client demande a entrez nom et l'envoi au server

--- client.c
+++ client.c
@@ -9,16 +9,21 @@ int main(int argc, char *argv[])
 
   printf("Connecting to echo...\n");
   zsock_t *req = zsock_new(ZMQ_REQ);
-  char* name = "Client1";
 
-  zsock_set_identity(req, name);
-  zsock_connect(req, "tcp://localhost:%s", argv[1]);
-  zstr_sendf(req, "%s", name);
+  while (!zsys_interrupted) {
+    char name[1024];
+    printf("Entrez un nom: ");
+    if (!fgets(name, 1024, stdin)) {
+      break;
+    }
+    zsock_set_identity(req, name);
+    zsock_connect(req, "tcp://localhost:%s", argv[1]);
+    zstr_sendf(req, "%s", name);
 
-  char *message = zstr_recv(req);
-  printf("Received : %s\n", message);
-  zstr_free(&message);
-
+    char *message = zstr_recv(req);
+    printf("Received : %s\n", message);
+    zstr_free(&message);
+}
   zsock_destroy(&req);
   return 0;
 }
