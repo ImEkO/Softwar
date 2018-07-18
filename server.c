af89fc26da2b5538029471c8685b70e1faaed362
Thierry Loris <thierr_l@etna-alternance.net>
2018-07-18 15:10:08 +0200

    README + Integration osir to server

--- server.c
+++ server.c
@@ -7,11 +7,29 @@ void *thread_1(void *arg)
 {
     printf("%s\n", "tageueule");
     while (!zsys_interrupted) {
-    char *message = zstr_recv(arg);
-    printf("Received: %s\n", message);
+    zmsg_t *message = zmsg_recv(router);
+
+    zframe_t *identity = zmsg_pop(message);
+    zframe_t *identity2 = zframe_from("test");
+    zframe_t *empty = zmsg_pop(message);
+    zframe_t *content = zmsg_pop(message);
+
+    zmsg_destroy(&message);
+    printf("Content of message is : %s\n %s\n", zframe_strdup(content),zframe_strdup(identity));
     sleep(1);
-    zstr_sendf(arg, "%s", message);
-    zstr_free(&message);
+
+    zmsg_t *response = zmsg_new();
+
+    zmsg_prepend(response, &identity);
+    zmsg_append(response, &empty);
+    zmsg_append(response, &content);
+
+    zmsg_send(&response, router);
+    zmsg_destroy(&response);
+
+    zframe_destroy(&identity);
+    zframe_destroy(&empty);
+    zframe_destroy(&content);
   }
     return 0;
 
@@ -29,10 +47,10 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  zsock_t *responder = zsock_new(ZMQ_REP);
-  zsock_bind(responder, "tcp://*:%s", argv[1]);
+  zsock_t *router = zsock_new(ZMQ_ROUTER);
+  zsock_bind(router, "tcp://*:5555");
 
-  if (pthread_create(&thread1, NULL, thread_1, responder)) {
+  if (pthread_create(&thread1, NULL, thread_1, router)) {
   perror("pthread_create");
   return EXIT_FAILURE;
     }
