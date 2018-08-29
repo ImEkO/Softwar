c84e9519a4947e17c28d302bb3bbfbda6695d157
Thierry Loris <thierr_l@etna-alternance.net>
2018-08-29 22:14:00 +0200

    Commit By  Loris
    client.js

--- Client/client.js
+++ Client/client.js
@@ -1,7 +1,7 @@
 var zmq       = require('zmq')
   , requester = zmq.socket('req');
 
-const readline = require('readline');
+/*const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -14,9 +14,13 @@ rl.on('line', (line) => {
   var split = line.split("|");
   console.log(split);
   switch (split[0]) {
-    case 'Identify':
+    case 'identify':
       console.log('Identify');
-      identify(split[1]);
+      identify(split[0], split[1], line);
+      break;
+    case 'jump':
+      console.log('Jump');
+      jump(split[0], split[1], line);
       break;
     default:
       console.log("No identify");
@@ -26,9 +30,21 @@ rl.on('line', (line) => {
 }).on('close', () => {
   console.log('Game Over');
   process.exit(0);
-});
+});*/
 
-function identify(id)
+var identifier = "OXO"+random()
+identify("identify", identifier, "identify|"+identifier);
+switch (urand()) {
+  case 1:
+    console.log('Jump');
+    jump("jump", "null", "jump|null");
+    break;
+  default:
+    console.log("No identify");
+    break;
+}
+
+function identify(cmd, id, line)
 {
   requester.identity = id
   requester.connect('tcp://localhost:3030');
@@ -39,6 +55,21 @@ function identify(id)
   });
 
   for (var i = 0; i < 10; ++i) {
-    requester.send("Hello");
+    requester.send(line);
+  }
+}
+
+function jump(cmd, id, line)
+{
+  requester.identity = id
+  requester.connect('tcp://localhost:3030');
+  var replyNbr = 0;
+  requester.on('message', function(msg) {
+  console.log('got reply', replyNbr, msg.toString());
+  replyNbr += 1;
+  });
+
+  for (var i = 0; i < 2; ++i) {
+    requester.send(line);
   }
 }
