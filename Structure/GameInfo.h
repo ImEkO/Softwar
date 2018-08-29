44666a76d23ba3fc1b703cb6fde4324cf883df32
Thierry Loris <thierr_l@etna-alternance.net>
2018-08-29 22:13:19 +0200

    Commit By  Loris
    .h

--- Structure/GameInfo.h
+++ Structure/GameInfo.h
@@ -1,8 +1,43 @@
-struct GameInfo {
-  //energy_cells: list<EnergyCell>
+#include <czmq.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+typedef struct s_Energycell EnergyCell;
+struct s_Energycell
+{
+    EnergyCell *next;
+    int   x;
+    int   y;
+    int   value;
+};
+
+typedef struct s_player Player;
+struct s_player
+{
+    char* name; /* pointeur sur une donnée générique */
+    int x;
+    int y;
+    int energy;
+    int looking;
+};
+
+typedef struct s_player_list PlayerList;
+struct s_player_list
+{
+  Player *player1;
+  Player *player2;
+  Player *player3;
+  Player *player4;
+};
+
+struct GameInfo {
   int map_size;
   int game_status;
-  char* players[4];
-  //energy_cells liste chainée
+  //char* players[4];
+  EnergyCell *energy_cell;
+  PlayerList *player_list;
 };
+
+int  pointeur_fonction(char* c);
