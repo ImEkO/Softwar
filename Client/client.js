6d34d194fd544197b7350448a81ec9ab12de2703
Unknown <olivie_a@etna-alternance.net>
2018-08-30 11:01:43 +0200

    add file for mouvement function and switch case done

--- Client/client.js
+++ Client/client.js
@@ -1,3 +1,5 @@
+var extfile = require("./function_mouvement.js")
+
 var zmq       = require('zmq')
   , requester = zmq.socket('req');
 
@@ -34,7 +36,7 @@ rl.on('line', (line) => {
 
 var identifier = "OxO"+randomIntInc(1, 10)
 identify("identify", identifier, "identify|"+identifier);
-switch (randomIntInc(1, 3)) {
+switch (randomIntInc(1, 13)) {
   case 1:
     console.log('Jump');
     jump("jump", "null", "jump|null");
@@ -47,69 +49,49 @@ switch (randomIntInc(1, 3)) {
     console.log('Backward');
     jump("backward", "null", "backward|null");
     break;
+  case 4:
+    console.log('leftfwd');
+    jump("leftfwd", "null", "leftfwd|null");
+    break;
+  case 5:
+    console.log('rightfwd');
+    jump("rightfwd", "null", "rightfwd|null");
+    break;
+  case 6:
+    console.log('right');
+    jump("right", "null", "right|null");
+    break;
+  case 7:
+    console.log('left');
+    jump("left", "null", "left|null");
+    break;
+  case 8:
+    console.log('looking');
+    jump("looking", "null", "looking|null");
+    break;
+  case 9:
+    console.log('gather');
+    jump("gather", "null", "gather|null");
+    break;
+  case 10:
+    console.log('watch');
+    jump("watch", "null", "watch|null");
+    break;
+  case 11:
+    console.log('attack');
+    jump("attack", "null", "attack|null");
+    break;
+  case 12:
+    console.log('selfid');
+    jump("selfid", "null", "selfid|null");
+    break;
+  case 13:
+    console.log('selfstats');
+    jump("selfstats", "null", "selfstats|null");
+    break;
   default:
     console.log("No identify");
     break;
-}
-
-function identify(cmd, id, line)
-{
-  requester.identity = id
-  requester.connect('tcp://localhost:3030');
-  var replyNbr = 0;
-  requester.on('message', function(msg) {
-  console.log('got reply', replyNbr, msg.toString());
-  replyNbr += 1;
-  });
-
-  for (var i = 0; i < 10; ++i) {
-    requester.send(line);
-  }
-}
-
-function jump(cmd, id, line)
-{
-  requester.identity = id
-  requester.connect('tcp://localhost:3030');
-  var replyNbr = 0;
-  requester.on('message', function(msg) {
-  console.log('got reply', replyNbr, msg.toString());
-  replyNbr += 1;
-  });
-
-  for (var i = 0; i < 2; ++i) {
-    requester.send(line);
-  }
-}
-
-function forward(cmd, id, line)
-{
-  requester.identity = id
-  requester.connect('tcp://localhost:3030');
-  var replyNbr = 0;
-  requester.on('message', function(msg) {
-  console.log('got reply', replyNbr, msg.toString());
-  replyNbr += 1;
-  });
-
-  for (var i = 0; i < 2; ++i) {
-    requester.send(line);
-  }
-}
-
-function backward(cmd, id, line)
-{
-  requester.identity = id
-  requester.connect('tcp://localhost:3030');
-  var replyNbr = 0;
-  requester.on('message', function(msg) {
-  console.log('got reply', replyNbr, msg.toString());
-  replyNbr += 1;
-  });
-
-  for (var i = 0; i < 2; ++i) {
-    requester.send(line);
-  }
 }
 
 function randomIntInc(low, high) {
